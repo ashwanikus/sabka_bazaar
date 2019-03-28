3ebd1cef6658f529235fc6cc91f553ad7ef5e6b2
Ashwani Kumar <ashwani.bietjhs@gmail.com>
2019-03-28 15:31:58 +0530

    webpack setup done

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+const header = `<nav>
+<div class="row">
+    <ul class="first_menu">
+        <li>Signin</li>
+        <li>Register</li>
+    </ul>
+</div>
+<div class="row">
+    <div><img src="assets/images/logo.png" alt="Logo" class="logo"></div>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+        </ul>
+    </div>
+</div>
+</nav>`;
+
+
+const footer = ``;
+
+(function () {
+    //document.getElementById("header").innerHTML = header;
+})();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,31 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const cssOutput = 'css/style.css';
 
 module.exports = {
-    entry: './resources/js/'
+    entry: ["./src/js/app.js", "./src/scss/home.scss"],
+    output: {
+        path: path.resolve(__dirname, "dist/assets"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin(cssOutput)
+    ]
 };
